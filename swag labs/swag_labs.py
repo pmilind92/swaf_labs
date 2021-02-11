563af8be1ea22f50e63326955deb323bea0c0e17
Milind Patil <pmilind92@gmail.com>
2021-02-11 18:30:26 +0530

    Update swag_labs.py

--- swag labs/swag_labs.py
+++ swag labs/swag_labs.py
@@ -4,10 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# PATH = "C:\chromedriver\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=r'.\driver\chromedriver.exe') #driver path
 driver.get("https://www.saucedemo.com/inventory.html")
-# link = driver.find_element_by_link_text ("ADD TO CART")
 time.sleep(2)
 driver.find_element_by_class_name('btn_primary.btn_inventory').click()
 driver.find_element_by_class_name('btn_primary.btn_inventory').click()
@@ -21,13 +19,4 @@ last = driver.find_element_by_id('last-name').send_keys('Patil')
 zip = driver.find_element_by_id('postal-code').send_keys('400605')
 driver.find_element_by_class_name('btn_primary.cart_button').click()
 time.sleep(10)
-#driver.close()
-
-
-
-
-
-
-# link.click()
-
 #driver.quit()
